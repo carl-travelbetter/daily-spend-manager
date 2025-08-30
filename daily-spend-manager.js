9bd3bfdd16ee60072983fd88b69467beebf3494a
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-08-30 15:49:15 +0100

    Update daily-spend-manager.js

--- daily-spend-manager.js
+++ daily-spend-manager.js
@@ -1,1 +1,23 @@
+function simpleCalculation()
+{
+   console.log("Simple Calculation");
+}
 
+function advancedCalculation()
+{
+   console.log("Advanced Calculation");
+}
+
+function loadSimpleTool()
+{
+  console.log("Load Simple Tool");
+   document.getElementById("advancedapp").hidden = true;
+   document.getElementById("simpleapp").hidden = false;
+}
+
+function loadAdvanceTool()
+{
+  console.log("Load Advanced Tool");
+   document.getElementById("advancedapp").hidden = false;
+   document.getElementById("simpleapp").hidden = true;
+}
